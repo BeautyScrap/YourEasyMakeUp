378b75d49eefa43b5638533f5f18434984d913f2
Mansonita <mansonita20@gmail.com>
2023-08-16 15:47:22 +0200

    add ControllerBase

--- YourEasyRent/Controllers/MongoController.cs
+++ YourEasyRent/Controllers/MongoController.cs
@@ -5,7 +5,7 @@ namespace YourEasyRent.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class MongoController : Controller
+    public class MongoController : ControllerBase
     {
         private readonly MongoCollection _mongoCollection;
         public MongoController(MongoCollection mongoCollection) => _mongoCollection = mongoCollection;  
