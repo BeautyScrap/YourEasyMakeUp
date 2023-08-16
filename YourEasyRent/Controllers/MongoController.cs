b87ba608c4ea7794cfc721862dbe68d537d7e0b2
Mansonita <mansonita20@gmail.com>
2023-08-16 15:46:38 +0200

    started writing MongoController

---
+++ YourEasyRent/Controllers/MongoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourEasyRent.DataBase;
+
+namespace YourEasyRent.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MongoController : Controller
+    {
+        private readonly MongoCollection _mongoCollection;
+        public MongoController(MongoCollection mongoCollection) => _mongoCollection = mongoCollection;  
+       
+    }
+}
