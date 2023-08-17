97ab26083e86f72c284fcd47eeb0c69364f28390
Mansonita <mansonita20@gmail.com>
2023-08-17 18:45:36 +0200

    Create an IProductRepository repository interface with methods

---
+++ YourEasyRent/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using YourEasyRent.Entities;
+
+namespace YourEasyRent.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts(); // IEnumerable - получение коллекции элементов 
+        Task<Product> Get(string id);
+        Task<IEnumerable<Product>> GetByBrand(string brand);
+        Task<IEnumerable<Product>> GetByName(string name);
+        Task<IEnumerable<Product>> GetByCategory(string category);
+        Task Create(Product product);
+        Task<IEnumerable<string>> CreateMany(IEnumerable<Product> products);
+        Task<bool> Update(Product product); // bool- если запить будет добвлена, то возвращается true,  если нет, то false
+        Task<bool> Delete(Product product);   
+
+
+
+
+
+
+
+    }
+}
