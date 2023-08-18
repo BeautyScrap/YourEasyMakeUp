f5a932718d62190e82c9712f0220e0003cd64979
Mansonita <mansonita20@gmail.com>
2023-08-18 17:26:11 +0200

    renamed to IProductRepository and created tasks for function signature

---
+++ YourEasyRent/DataBase/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using YourEasyRent.Entities;
+
+namespace YourEasyRent.DataBase.Interfaces
+{
+    public interface IProductRepository
+    {
+        // сделать сигнатуры как было в iproductrepositor
+
+        Task<IEnumerable<Product>> GetProducts();//добавила операторы для CRUD  к IMongoCollection
+        Task<IEnumerable<Product>> GetById(string id);
+        Task<IEnumerable<Product>> GetBrand(string brand);
+        Task<IEnumerable<Product>> GetByName(int name);
+        Task Create(Product product);
+        Task<IEnumerable<string>> CreateMany(IEnumerable<Product> products);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+
+
+
+    }
+}
