150d4132022e48d69a64ccf982481ba7e5dd0ea2
Mansonita <mansonita20@gmail.com>
2023-08-21 18:45:39 +0200

    deleted collections of <IEnumerable> instances in IProductRepository

--- YourEasyRent/DataBase/Interfaces/IProductRepository.cs
+++ YourEasyRent/DataBase/Interfaces/IProductRepository.cs
@@ -7,14 +7,14 @@ namespace YourEasyRent.DataBase.Interfaces
     {
         // сделать сигнатуры как было в iproductrepositor
 
-        Task<IEnumerable<Product>> GetProducts();//добавила операторы для CRUD  к IMongoCollection
-        Task<IEnumerable<Product>> GetById(string id);
-        Task<IEnumerable<Product>> GetBrand(string brand);
-        Task<IEnumerable<Product>> GetByName(int name);
-        Task Create(Product product);
-        Task<IEnumerable<string>> CreateMany(IEnumerable<Product> products);
-        Task<bool> Update(Product product);
-        Task<bool> Delete(string id);
+        Task<IEnumerable<Product>> GetProducts();//добавила операторы для CRUD  к IMongoCollection, метод GET
+        Task<Product> GetById(string id);
+        Task<Product> GetByBrand(string brand);
+        Task<Product> GetByName(string name);
+        Task Create(Product newProduct); // метод POST
+        Task<IEnumerable<string>> CreateMany(IEnumerable<Product> products); // метод  POST
+        Task<bool> Update(Product product); // метод PUT
+        Task<bool> Delete(Product deleteProduct); // метод DELETE
 
 
 
