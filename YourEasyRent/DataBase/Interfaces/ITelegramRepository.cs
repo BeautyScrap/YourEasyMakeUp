9eaf46f324567d155d501bb134e3d845bece7cf1
Mansonita <mansonita20@gmail.com>
2023-10-05 15:27:09 +0200

    added telegram repository

---
+++ YourEasyRent/DataBase/Interfaces/ITelegramRepository.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB;
+using YourEasyRent.Entities;
+
+namespace YourEasyRent.DataBase.Interfaces
+{
+    public interface ITelegramRepository
+    {
+        Task<IEnumerable<Product?>> GetProductsByBrandAndPrice(string brand, decimal price);
+    }
+}
---
+++ YourEasyRent/DataBase/TelegramRepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB;
+using MongoDB.Driver;
+using Telegram.Bot;
+using YourEasyRent.DataBase.Interfaces;
+using YourEasyRent.Entities;
+
+namespace YourEasyRent.DataBase
+{
+    public class TelegramRepository : ITelegramRepository
+    {
+        public async Task<IEnumerable<Product>> GetProductsByBrandAndPrice(string brand, decimal price)
+        {
+            
+        }
+    }
+}
