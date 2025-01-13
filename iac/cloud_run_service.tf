e21e840350a00f701be9f138a10c3544e036a4c7
Mansonita <mansonita20@gmail.com>
2025-01-13 17:55:54 +0100

    dpl

--- iac/cloud_run_service.tf
+++ iac/cloud_run_service.tf
@@ -1,3 +1,4 @@
+/*
 resource "google_cloud_run_service" "bot" {
   name     = var.bot_name
   location = var.region
@@ -141,7 +142,7 @@ resource "google_cloud_run_service" "subscriber-api" {
 
   depends_on = [google_project_service.cloud_run_googleapis_com]
 }
-
+*/
 # Allow unauthenticated users to invoke the service
 resource "google_cloud_run_service_iam_member" "run_all_users" {
   service  = google_cloud_run_service.bot.name
